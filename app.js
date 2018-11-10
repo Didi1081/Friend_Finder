bc2f8fefec1b85c250453a22e28484a8166d8c61
Didi <ddapp108@gmail.com>
2018-11-10 14:26:01 -0600

    inital

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("dotenv");
+var path = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+
+};
