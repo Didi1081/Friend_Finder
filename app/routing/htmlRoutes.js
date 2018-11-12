e6dc9d33835a310e04ca0c0d616dfd76f8d38b1a
Didi <ddapp108@gmail.com>
2018-11-11 21:32:43 -0600

    updates

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,14 +5,12 @@ var path = require('path');
 
 
 
-
-
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
 
 });
 
-router.get('/survey', function(req,res){
+router.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
 })
 
